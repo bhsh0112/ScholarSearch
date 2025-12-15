2a7ac98ae9b32058db3e0cc5e5c288b155588b31
bhsh0112 <bhsh0112@163.com>
2025-12-15 19:01:01 +0800

    修复

---
+++ web/types/nodemailer.d.ts
@@ -0,0 +1,12 @@
+/**
+ * nodemailer 类型声明占位。
+ *
+ * 如果未来需要更完善的类型，可以安装：
+ *   npm install --save-dev @types/nodemailer
+ */
+declare module "nodemailer" {
+  const nodemailer: any;
+  export default nodemailer;
+}
+
+
